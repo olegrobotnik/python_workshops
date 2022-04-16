8f19663fb46a994fe74e21a7985225377424db99
Oleg Mukhin <oleg.robotnik@gmail.com>
2022-04-16 09:30:17 +0300

    Add solved task_01

---
+++ task_01_sqrd_or_not.py
@@ -0,0 +1,7 @@
+# По двум заданным числам проверить является ли одно квадратом второго
+
+# a=3
+# b=9
+
+a, b = map(int, input('Enter numbers space separated: ').split())
+print(bool(a == b ** 2 or b == a ** 2))
