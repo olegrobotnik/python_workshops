a7e24080109a437528d6bfe4099ab79216dfca60
Oleg Mukhin <oleg.robotnik@gmail.com>
2022-04-16 09:50:17 +0300

    Add solved task_05

---
+++ task_05_check_multiplicity.py
@@ -0,0 +1,5 @@
+# Дано число. Проверить кратно ли оно 5 и 10 или 15 но не 30
+
+n = int(input('Enter a whole number: '))
+
+print(bool(n % 5 == 0 and n % 10 == 0 or n % 15 == 0) and not n % 30 == 0)
