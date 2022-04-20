d4f305f9e6621c8ae3cbb50c7a73b63659aab602
Oleg Mukhin <oleg.robotnik@gmail.com>
2022-04-20 19:48:35 +0300

    Add another solution in task_14

--- task_14_float_number_digits_sum.py
+++ task_14_float_number_digits_sum.py
@@ -1,5 +1,14 @@
 # Подсчитать сумму цифр в вещественном числе.
 
-string_number = str(float(input('Enter a real number: '))).replace('.', '')
-list_number = [int(a) for a in string_number]
-print(f'Sum of all digits in the real number is {sum(list_number)}.')
+n = float(input('Enter a real number: '))
+str_n = str(n).replace('.', '')
+list_n = [int(item) for item in str_n]
+dig_sum = 0
+for item in list_n:
+    dig_sum += item
+
+print(f'Sum of all digits in the real number is {dig_sum}.')
+
+# string_number = str(float(input('Enter a real number: '))).replace('.', '')
+# list_number = [int(a) for a in string_number]
+# print(f'Sum of all digits in the real number is {sum(list_number)}.')
