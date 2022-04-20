202662004dcc7d7de8c8c33be7d15604094dde0e
Oleg Mukhin <oleg.robotnik@gmail.com>
2022-04-20 20:20:28 +0300

    Add solved task_16

---
+++ task_16_sequence_sum.py
@@ -0,0 +1,9 @@
+# Задать список из n чисел последовательности (1+1/n)**n и вывести
+# на экран их сумму
+
+n = int(input('Enter a number: '))
+list_n = []
+for m in range(1, n + 1):
+    list_n.append((1 + 1 / m) ** m)
+    print((1 + 1 / m) ** m)
+print(f'Sum of numbers in sequence is: {(round(sum(list_n), 2))}')
