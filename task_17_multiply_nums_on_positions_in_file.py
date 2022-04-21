9ba94add539682146e381dda7ec5847b4a3fb089
Oleg Mukhin <oleg.robotnik@gmail.com>
2022-04-21 17:06:39 +0300

    Add solved task_17

--- task_17_multiply_nums_on_positions_in_file.py
+++ task_17_multiply_nums_on_positions_in_file.py
@@ -1,28 +1,23 @@
 # Задать список из N элементов, заполненных числами из [-N, N]. Найти произведение элементов на указанных позициях.
 # Позиции хранятся в файле positions.txt в одной строке одно число
 
-N = 7
+
+N = abs(int(input('Enter a number from 1 to 9: ')))
+# N = 7
 sequence = []
 
 for j in range(-N, N + 1):
     sequence.append(j)
-print('\n\tIndex:')
-for y in range(0, len(sequence)):
+print('\n\tPosition:')
+for y in range(1, len(sequence) + 1):
     print(f"\t{y}", end=' ')
 print('\n\tSequence:')
 for i in sequence:
     print(f"\t{i}", end=' ')
 
-
-print()
-# print(sequence)
-
-# N = abs(int(input('Enter a number: ')))
-
-
 file = open('positions.txt', 'w')
 
-positions = [1, 2, 3, 4, 5]
+positions = [3, 13, 15]
 for i in positions:
     file.write('%d \n' % i)
 file.close()
@@ -33,5 +28,14 @@ content = file.readlines()
 for line in content:
     pos_list.append(int(line))
 
-# print(pos_list)
-# print(line)
+print('\n\tMultiplied positions from .txt file: ')
+for k in pos_list:
+    print(f"\t{k}", end=' ')
+
+selected_list = [sequence[i - 1] for i in pos_list]
+print(f"\n\t{selected_list}")
+result = 1
+for m in selected_list:
+    result = result * m
+print('\tProduct of numbers: ')
+print(f"\t{result}")
