6f224dd0ad7ab6ab430abf7486d722a3e90f836d
Oleg Mukhin <oleg.robotnik@gmail.com>
2022-04-21 21:58:19 +0300

    Add recursive solution in task_15

--- task_15_1_to_N_numbers_product.py
+++ task_15_1_to_N_numbers_product.py
@@ -1,12 +1,25 @@
 # Написать программу получающую набор произведений чисел от 1 до N.
 # Пример: пусть N = 4, тогда [ 1, 2, 6, 24 ]
 
+# N = int(input('Enter a number of terms: '))
+#
+# factorial = 1
+# for i in range(1, N + 1):
+#     factorial = factorial * i
+#     if i < N:
+#         print(factorial, end=', ')
+#     else:
+#         print(factorial, end=', ...')
+
 N = int(input('Enter a number of terms: '))
 
-factorial = 1
-for i in range(1, N + 1):
-    factorial = factorial * i
-    if i < N:
-        print(factorial, end=', ')
+def factorial(N):
+    if N == 0:
+        return 1
     else:
-        print(factorial, end=', ...')
+        x = N * factorial(N - 1)
+        print(f'{x}', end=' ')
+        return x
+
+factorial(N)
+
