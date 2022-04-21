2ddfdab2b8bd5243e55ebd6a0d2f1db5af4bc443
Oleg Mukhin <oleg.robotnik@gmail.com>
2022-04-21 22:08:45 +0300

    Minor code edits in task_16

--- task_16_sequence_sum.py
+++ task_16_sequence_sum.py
@@ -5,5 +5,5 @@ n = int(input('Enter a number: '))
 list_n = []
 for m in range(1, n + 1):
     list_n.append((1 + 1 / m) ** m)
-    print((1 + 1 / m) ** m)
-print(f'Sum of numbers in sequence is: {(round(sum(list_n), 2))}')
+    print((1 + 1 / m) ** m, end=' ')
+print(f'\nSum of numbers in sequence is: {(round(sum(list_n), 2))}')
