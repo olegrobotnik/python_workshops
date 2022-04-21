c8f32dc51267dbde891450c0f5f9ef906700addc
Oleg Mukhin <oleg.robotnik@gmail.com>
2022-04-21 13:52:13 +0300

    Add list fill functionality in task_17

--- task_17_multiply_nums_on_positions_in_file.py
+++ task_17_multiply_nums_on_positions_in_file.py
@@ -1,20 +1,27 @@
 # Задать список из N элементов, заполненных числами из [-N, N]. Найти произведение элементов на указанных позициях.
 # Позиции хранятся в файле positions.txt в одной строке одно число
 
-# N = int(input('Enter a number: '))
+N = 7
+sequence = []
+for j in range(-N, N + 1):
+    sequence.append(j)
+print(sequence)
 
-file = open('positions.txt', 'w')
+# N = abs(int(input('Enter a number: ')))
 
-data = [1, 2, 3, 4, 5]
-for i in data:
-    file.write('%d \n' % i)
-file.close()
 
-pos_list = []
-file = open('positions.txt', 'r')
-content = file.readlines()
-for line in content:
-    pos_list.append(int(line))
-
-print(pos_list)
+# file = open('positions.txt', 'w')
+#
+# data = [1, 2, 3, 4, 5]
+# for i in data:
+#     file.write('%d \n' % i)
+# file.close()
+#
+# pos_list = []
+# file = open('positions.txt', 'r')
+# content = file.readlines()
+# for line in content:
+#     pos_list.append(int(line))
+#
+# print(pos_list)
 # print(line)
