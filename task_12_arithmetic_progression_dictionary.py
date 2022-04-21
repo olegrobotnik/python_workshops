232bbc8df271e078e0d9bb47755fb6b8a8631bcd
Oleg Mukhin <oleg.robotnik@gmail.com>
2022-04-21 20:31:42 +0300

    Add another solution in task_12

--- task_12_arithmetic_progression_dictionary.py
+++ task_12_arithmetic_progression_dictionary.py
@@ -2,9 +2,12 @@
 # последовательности 3n + 1. Для n = 6: {1: 4, 2: 7, 3: 10, 4: 13, 5: 16, 6: 19}
 
 N = abs(int(input('Enter a number of terms: ')))
-dictionary = {}
+# dictionary = {}
+#
+# for i in range(1, N + 1):
+#     dictionary[i] = 3 * i + 1
+#
+# print(dictionary)
 
-for i in range(1, N + 1):
-    dictionary[i] = 3 * i + 1
-
-print(dictionary)
+dic = {n: 3 * n + 1 for n in range(1, N)}
+print(dic)
