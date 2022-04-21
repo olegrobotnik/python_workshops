df5bcdda44b445fcf5503e304ff2d43d27e81acc
Oleg Mukhin <oleg.robotnik@gmail.com>
2022-04-21 14:07:49 +0300

    Add print list and item index functionality in task_17

--- task_17_multiply_nums_on_positions_in_file.py
+++ task_17_multiply_nums_on_positions_in_file.py
@@ -3,25 +3,35 @@
 
 N = 7
 sequence = []
+
 for j in range(-N, N + 1):
     sequence.append(j)
-print(sequence)
+print('\n\tIndex:')
+for y in range(0, len(sequence)):
+    print(f"\t{y}", end=' ')
+print('\n\tSequence:')
+for i in sequence:
+    print(f"\t{i}", end=' ')
+
+
+print()
+# print(sequence)
 
 # N = abs(int(input('Enter a number: ')))
 
 
-# file = open('positions.txt', 'w')
-#
-# data = [1, 2, 3, 4, 5]
-# for i in data:
-#     file.write('%d \n' % i)
-# file.close()
-#
-# pos_list = []
-# file = open('positions.txt', 'r')
-# content = file.readlines()
-# for line in content:
-#     pos_list.append(int(line))
-#
+file = open('positions.txt', 'w')
+
+positions = [1, 2, 3, 4, 5]
+for i in positions:
+    file.write('%d \n' % i)
+file.close()
+
+pos_list = []
+file = open('positions.txt', 'r')
+content = file.readlines()
+for line in content:
+    pos_list.append(int(line))
+
 # print(pos_list)
 # print(line)
