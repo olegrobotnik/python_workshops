59f12d287443998d01f34f051cfb4afae3ddfb55
Oleg Mukhin <oleg.robotnik@gmail.com>
2022-05-25 22:26:41 +0300

    Add another solution using list comprehension & lambda functions in task_16

--- task_16_sequence_sum.py
+++ task_16_sequence_sum.py
@@ -2,8 +2,12 @@
 # на экран их сумму
 
 n = int(input('Enter a number: '))
-list_n = []
-for m in range(1, n + 1):
-    list_n.append((1 + 1 / m) ** m)
-    print((1 + 1 / m) ** m, end=' ')
-print(f'\nSum of numbers in sequence is: {(round(sum(list_n), 2))}')
+print(*[((1 + 1 / m) ** m) for m in range(1, n + 1)])
+print(f'Sum of numbers in sequence is: {(round(sum(list(map(lambda m: ((1 + 1 / m) ** m), range(1, n + 1)))), 3))}')
+
+# n = int(input('Enter a number: '))
+# list_n = []
+# for m in range(1, n + 1):
+#     list_n.append((1 + 1 / m) ** m)
+#     print((1 + 1 / m) ** m, end=' ')
+# print(f'\nSum of numbers in sequence is: {(round(sum(list_n), 2))}')
