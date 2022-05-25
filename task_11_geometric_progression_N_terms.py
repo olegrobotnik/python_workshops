c97eae9dbb354af3b940d8840c7adedb6dcff5fa
Oleg Mukhin <oleg.robotnik@gmail.com>
2022-05-25 19:59:35 +0300

    Add second solution using list comprehension in task_11

--- task_11_geometric_progression_N_terms.py
+++ task_11_geometric_progression_N_terms.py
@@ -1,12 +1,12 @@
 # Сформировать список из N членов последовательности.
 # Для N = 5: 1, -3, 9, -27, 81 и т.д.
 
-N = abs(int(input('Enter a number of terms: ')))
+print(*[((-3) ** i) for i in range(int(input('Enter a number of terms: ')))])
 
-sec_list = []
-for i in range(N):
-    sec_list.append((-3) ** i)
-    print(f'\t{sec_list[i]}', end=' ')
+# sec_list = []
+# for i in range(N):
+#     sec_list.append((-3) ** i)
+#     print(f'\t{sec_list[i]}', end=' ')
 
 # for i in range(0, N + 1):
 #     if i < N:
