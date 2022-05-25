802d30e7739e1df932ec22a69afe1be692874355
Oleg Mukhin <oleg.robotnik@gmail.com>
2022-05-25 23:37:02 +0300

    Add another solution using list comprehension function in task_17

--- task_17_multiply_nums_on_positions_in_file.py
+++ task_17_multiply_nums_on_positions_in_file.py
@@ -3,49 +3,53 @@
 
 import numpy as np
 
-N = abs(int(input('\tEnter a number from 1 to 9: ')))
-# N = 7
-sequence = []
+N = abs(int(input('Enter a number from 1 to 9: ')))
 
-for j in range(-N, N + 1):
-    sequence.append(j)
-print('\n\tPosition:')
-for y in range(1, len(sequence) + 1):
-    print(f"\t{y}", end=' ')
-print('\n\tSequence:')
-for i in sequence:
-    print(f"\t{i}", end=' ')
-
-file = open('task_17_positions.txt', 'w')
-
-positions = list(map(int, input('\n\tSelect positions to multiply: ').split()))
-
-# positions = [3, 13, 15]
-for i in positions:
-    file.write('%d \n' % i)
-file.close()
-
-pos_list = []
+sequence = [i for i in range(-N, N + 1)]
 file = open('task_17_positions.txt', 'r')
-content = file.readlines()
-for line in content:
-    pos_list.append(int(line))
+positions_list = [int(line) for line in file.readlines()]
+print(*sequence)
+print(*positions_list)
+product = np.prod([sequence[i] for i in positions_list])
+print(product)
 
-print('\n\tMultiplied positions from .txt file: ')
-for k in pos_list:
-    print(f"\t{k}", end=' ')
+# import numpy as np
+#
+# N = abs(int(input('\tEnter a number from 1 to 9: ')))
 
-print()
-selected_list = [sequence[i - 1] for i in pos_list]
-for p in selected_list:
-    print(f"\t{p}", end=' ')
-
-print(f"\n\t{selected_list}")
-
-# product = 1                       # Цикл умножения
-# for m in selected_list:
-#     product = product * m
-
-print(f"\n\tProduct of numbers: {np.prod(selected_list)}") # Цикл умножения элементов списка с помощью библиотеки numpy
-
-# print(f"\n\tProduct of numbers: {product}")
+# sequence = []
+#
+# for j in range(-N, N + 1):
+#     sequence.append(j)
+# print('\n\tPosition:')
+# for y in range(1, len(sequence) + 1):
+#     print(f"\t{y}", end=' ')
+# print('\n\tSequence:')
+# for i in sequence:
+#     print(f"\t{i}", end=' ')
+#
+# file = open('task_17_positions.txt', 'w')
+#
+# positions = list(map(int, input('\n\tSelect positions to multiply: ').split()))
+#
+# for i in positions:
+#     file.write('%d \n' % i)
+# file.close()
+#
+# pos_list = []
+# file = open('task_17_positions.txt', 'r')
+# content = file.readlines()
+# for line in content:
+#     pos_list.append(int(line))
+#
+# print('\n\tMultiplied positions from .txt file: ')
+# for k in pos_list:
+#     print(f"\t{k}", end=' ')
+#
+# print()
+# selected_list = [sequence[i - 1] for i in pos_list]
+# for p in selected_list:
+#     print(f"\t{p}", end=' ')
+#
+# print(f"\n\t{selected_list}")
+# print(f"\n\tProduct of numbers: {np.prod(selected_list)}")  # Цикл умножения элементов списка с помощью библиотеки numpy
