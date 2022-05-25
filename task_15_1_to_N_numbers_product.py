507d092ea72ed33601373f91be34e3d479662799
Oleg Mukhin <oleg.robotnik@gmail.com>
2022-05-25 21:54:34 +0300

    Add another solution using map & lambda functions in task_15

--- task_15_1_to_N_numbers_product.py
+++ task_15_1_to_N_numbers_product.py
@@ -1,6 +1,32 @@
 # Написать программу получающую набор произведений чисел от 1 до N.
 # Пример: пусть N = 4, тогда [ 1, 2, 6, 24 ]
 
+from math import factorial
+
+N = int(input('Enter a number of terms: '))
+print(*list(map(lambda i: factorial(i), range(1, N + 1))))
+
+# def factorial(N):
+#     if N == 0:
+#         return 1
+#     else:
+#         x = N * factorial(N - 1)
+#         print(f'{x}', end=' ')
+#         return x
+#
+#
+# N = int(input('Enter a number of terms: '))
+# factorial(N)
+
+
+# N = int(input('Enter a number of terms: '))
+# factorial(N)
+# N= 5
+# factorial = 1
+# facto = [factorial * i for i in range(1, N+1)]
+# print(facto)
+
+
 # N = int(input('Enter a number of terms: '))
 #
 # factorial = 1
@@ -10,16 +36,3 @@
 #         print(factorial, end=', ')
 #     else:
 #         print(factorial, end=', ...')
-
-N = int(input('Enter a number of terms: '))
-
-def factorial(N):
-    if N == 0:
-        return 1
-    else:
-        x = N * factorial(N - 1)
-        print(f'{x}', end=' ')
-        return x
-
-factorial(N)
-
