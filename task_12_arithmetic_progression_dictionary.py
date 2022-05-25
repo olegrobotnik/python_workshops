5caa2541c78e19a231090897900abb9d2489a60e
Oleg Mukhin <oleg.robotnik@gmail.com>
2022-05-25 20:02:41 +0300

    Minor code edits in task_12

--- task_12_arithmetic_progression_dictionary.py
+++ task_12_arithmetic_progression_dictionary.py
@@ -1,13 +1,11 @@
 # Для натурального n создать словарь индекс-значение, состоящий из элементов
 # последовательности 3n + 1. Для n = 6: {1: 4, 2: 7, 3: 10, 4: 13, 5: 16, 6: 19}
 
-N = abs(int(input('Enter a number of terms: ')))
+print({n: 3 * n + 1 for n in range(1, abs(int(input('Enter a number of terms: '))))})
+
 # dictionary = {}
 #
 # for i in range(1, N + 1):
 #     dictionary[i] = 3 * i + 1
 #
 # print(dictionary)
-
-dic = {n: 3 * n + 1 for n in range(1, N)}
-print(dic)
