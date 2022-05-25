90b06e62587a859216d2abb095b4d747da378705
Oleg Mukhin <oleg.robotnik@gmail.com>
2022-05-25 20:32:22 +0300

    Add second solution using map function in task_14

--- task_14_float_number_digits_sum.py
+++ task_14_float_number_digits_sum.py
@@ -1,14 +1,17 @@
 # Подсчитать сумму цифр в вещественном числе.
 
-n = float(input('Enter a real number: '))
-str_n = str(n).replace('.', '')
-list_n = [int(item) for item in str_n]
-dig_sum = 0
-for item in list_n:
-    dig_sum += item
-
-print(f'Sum of all digits in the real number is {dig_sum}.')
+dig_sum = map(int, str(float(input('Enter a real number: '))).replace('.', ''))
+print(f'Sum of all digits in the real number is {sum(dig_sum)}.')
 
 # string_number = str(float(input('Enter a real number: '))).replace('.', '')
 # list_number = [int(a) for a in string_number]
 # print(f'Sum of all digits in the real number is {sum(list_number)}.')
+
+# n = float(input('Enter a real number: '))
+# str_n = str(n).replace('.', '')
+# list_n = [int(item) for item in str_n]
+# dig_sum = 0
+# for item in list_n:
+#     dig_sum += item
+#
+# print(f'Sum of all digits in the real number is {sum(list_n)}.')
